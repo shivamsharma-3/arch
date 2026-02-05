d0ba1201602275e6db54bd1034bb16207d0bbc73
shivam-sharma52 <shivamsharma20179@gmail.com>
2026-02-05 15:36:00 +0530

    Add success page and wire Netlify redirect

--- pages/Success.tsx
+++ pages/Success.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { CheckCircle } from 'lucide-react';
 import Button from '../components/Button';
 import DepthCard from '../components/DepthCard';
