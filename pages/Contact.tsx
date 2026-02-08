ff69107e9ca8770e2ca652ee62909fc07b5a0aee
shivam-sharma52 <shivamsharma20179@gmail.com>
2026-02-08 23:28:12 +0530

    up

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -152,7 +152,7 @@ const Contact: React.FC = () => {
 
             <div className="space-y-1">
               <label className="text-[10px] font-bold uppercase tracking-widest text-neutral-500 ml-4">Message (Optional)</label>
-              <input 
+              <textarea 
                 name="message"
                 className="w-full px-5 py-4 rounded-2xl bg-black/40 border border-white/5 focus:border-teal-500/50 focus:ring-1 focus:ring-teal-500/20 transition-all outline-none text-white font-medium text-sm placeholder-neutral-700 min-h-[120px] resize-none"
                 placeholder="Briefly describe your current revenue friction or specific goals..."
